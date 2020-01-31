5e4a83a939cf16fc866c825cf124166f6f27ee17
qiaoli116 <qiaoli.116@gmail.com>
2020-02-01 10:39:33 +1100

    something is updated message

--- app/src/main/java/com/holmesglen/lab05multiscreenapp/Activities/HomeActivity.java
+++ app/src/main/java/com/holmesglen/lab05multiscreenapp/Activities/HomeActivity.java
@@ -12,5 +12,7 @@ public class HomeActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+
+        // something is updated
     }
 }
