e472197fce1d817d440fe21b7dca47c6801acc5d
qiaoli116 <qiaoli.116@gmail.com>
2020-02-01 11:12:37 +1100

    something is changed again

--- app/src/main/java/com/holmesglen/lab05multiscreenapp/Activities/HomeActivity.java
+++ app/src/main/java/com/holmesglen/lab05multiscreenapp/Activities/HomeActivity.java
@@ -14,5 +14,7 @@ public class HomeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_home);
 
         // something is updated
+
+        // something is updated again
     }
 }
